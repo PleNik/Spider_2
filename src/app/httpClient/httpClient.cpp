04aaf02a14183cd54ac9bb38ded6ee0998db9a90
PleNik <pleshkovn69@gmail.com>
2025-05-31 16:13:52 +0500

    init commit

---
+++ src/app/httpClient/httpClient.cpp
@@ -0,0 +1,8 @@
+#include "httpClient.h"
+
+int main() {
+	SetConsoleCP(CP_UTF8);
+	SetConsoleOutputCP(CP_UTF8);
+
+	std::cout << "Hello, Spider!" << std::endl;
+}
---
+++ src/app/httpClient/httpClient.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+#pragma execution_character_set("utf-8")
